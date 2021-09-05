7f24444aff2c2075b15573979506a989ec9d895f
Andreas Paridis <a.parides@student.reading.ac.uk>
2021-09-05 13:40:05 +0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FadeIn from 'react-fade-in/lib/FadeIn';
-import ClipLoader from 'react-spinners/ClipLoader';
 
-import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
 import ScrollToTop from './components/ScrollToTop';
-import Hero from './components/HeroSection';
-import About from './components/AboutSection';
-import Experience from './components/ExperienceSection';
-import Projects from './components/ProjectsSection';
 import Home from './pages/Home';
 
 export default function App() {
